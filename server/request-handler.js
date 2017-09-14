46d2f0f691f621aa95864e95c5ce46b2652cb372
Hack Reactor Student <student@hackreactor.com>
2017-09-14 15:43:26 -0700

    Refactor NODE.JS server to use Express library

--- server/request-handler.js
+++ server/request-handler.js
@@ -20,15 +20,20 @@ this file and include it in basic-server.js so that it actually works.
 //
 // Another way to get around this restriction is to serve you chat
 // client from this domain by setting up static file serving.
+const express = require('express');
+const app = express();
+
 var defaultCorsHeaders = {
   'access-control-allow-origin': '*',
   'access-control-allow-methods': 'GET, POST, PUT, DELETE, OPTIONS',
   'access-control-allow-headers': 'content-type, accept',
   'access-control-max-age': 10 // Seconds.
 };
+
 var messages = [];
 
-var getHandler = function (response, request) {
+var getHandler = function (request, response) {
+  console.log('Serving request type ' + request.method + ' for url ' + request.url);
   let parameterStringPart = request.url.split('?')[1];
   let parameters = {};
   if (parameterStringPart) {
@@ -53,7 +58,8 @@ var getHandler = function (response, request) {
   response.end(JSON.stringify({results: returnMessages}));
 };
 
-var postHandler = function (response, request) {
+var postHandler = function (request, response) {
+  console.log('Serving request type ' + request.method + ' for url ' + request.url);
   let headers = defaultCorsHeaders;
   headers['Content-Type'] = 'application/json';
   response.writeHead(201, headers);
@@ -71,80 +77,53 @@ var postHandler = function (response, request) {
   });
 };
 
-var optionsHandler = function (response) {
-  response.end();
-};
-
-var notFoundHandler = function (response) {
-  response.writeHead(404);
-  response.end();
-};
-
-var deleteHandler = function (response) {
-  response.writeHead(403);
-  response.end();
-};
-
-var actions = {'POST': postHandler, 'GET': getHandler, 'OPTIONS': optionsHandler, 'DELETE': deleteHandler};
-
-
-var requestHandler = function(request, response) {
-  // Request and Response come from node's http module.
-  //
-  // They include information about both the incoming request, such as
-  // headers and URL, and about the outgoing response, such as its status
-  // and content.
-  //
-  // Documentation for both request and response can be found in the HTTP section at
-  // http://nodejs.org/documentation/api/
-
-  // Do some basic logging.
-  //
-  // Adding more logging to your server can be an easy way to get passive
-  // debugging help, but you should always be careful about leaving stray
-  // console.logs in your code.
+var optionsHandler = function (request, response) {
   console.log('Serving request type ' + request.method + ' for url ' + request.url);
-
-  // The outgoing status.
-  var statusCode = 200;
-
-  // See the note below about CORS headers.
   var headers = defaultCorsHeaders;
-
-  // Tell the client we are sending them plain text.
-  //
-  // You will need to change this if you are sending something
-  // other than plain text, like JSON or HTML.
   headers['Content-Type'] = 'text/plain';
-
-  // .writeHead() writes to the request line and headers of the response,
-  // which includes the status and all headers.
-  response.writeHead(statusCode, headers);
-
-  let stringEndPoint = request.url.split('?')[0];
-  let endPointElements = stringEndPoint.split('/').splice(-2);
-  
-  if (endPointElements[0] !== 'classes') {
-    notFoundHandler(response);
-  }
-  if (endPointElements[1] !== 'room' && endPointElements[1] !== 'messages') {
-    notFoundHandler(response);
-  }
-
-  let action = actions[request.method];
-  action ? action(response, request) : notFoundHandler(response);
-  
-  
-
-  // Make sure to always call response.end() - Node may not send
-  // anything back to the client until you do. The string you pass to
-  // response.end() will be the body of the response - i.e. what shows
-  // up in the browser.
-  //
-  // Calling .end "flushes" the response's internal buffer, forcing
-  // node to actually send all the data over to the client.
-  //response.end(JSON.stringify({results: []}));
+  response.writeHead(200, headers);
+  response.end();
 };
 
+var notFoundHandler = function (request, response) {
+  console.log('Serving request type ' + request.method + ' for url ' + request.url);
+  var headers = defaultCorsHeaders;
+  headers['Content-Type'] = 'text/plain';
+  response.writeHead(404, headers);
+  response.end();
+};
 
-module.exports.requestHandler = requestHandler;
+var deleteHandler = function (request, response) {
+  console.log('Serving request type ' + request.method + ' for url ' + request.url);
+  var headers = defaultCorsHeaders;
+  headers['Content-Type'] = 'text/plain';
+  response.writeHead(403, headers);
+  response.end();
+};
+
+var path = require('path');
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.get('/chatterbox/classes/messages', (req, res) => {
+  getHandler(req, res);
+});
+
+app.post('/chatterbox/classes/messages', (req, res) => {
+  postHandler(req, res);
+});
+
+app.post('/chatterbox/classes/room', (req, res) => {
+  postHandler(req, res);
+});
+
+app.options('/*', (req, res) => {
+  optionsHandler(req, res);
+});
+
+app.delete('/*', (req, res) => {
+  deleteHandler(req, res);
+});
+
+module.exports = app;
+
